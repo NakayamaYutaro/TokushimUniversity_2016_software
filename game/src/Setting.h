e87779f1c724d1022c00a44bda0e3980c6e091ac
仲野　将史 <c501606903@clpc025.(none)>
2016-11-25 16:18:40 +0900

    add setting

---
+++ game/src/Setting.h
@@ -0,0 +1,22 @@
+#ifndef SETTING_H
+#define SETTING_H
+
+// ------ for images ------ //
+#define GAME_WINDOW_IMG_PATH "../../assets/ground.png"
+// ------ for images ------ //
+#define GAME_WINDOW_IMG_PATH "../../assets/ground2.png"
+// ------ for images ------ //
+#define GAME_WINDOW_IMG_PATH "../../assets/ground3.png"
+// ------ for images ------ //
+#define GAME_WINDOW_IMG PATH "../../assets/gorund4.png"
+// ----- for images ------ //
+#define GAME_WINDOW_IMG_PATH "../../assets/roomba.png"
+// ----- for images ------ //
+#define GAME_WINDOW_IMG_PATH "../../assets/roomba2.png"
+// ----- for images ------ //
+#define GAME_WINDOW_IMG_PATH "../../assets/roomba3.png"
+// ----- for images ------ //
+#define GAME_WINDOW_IMG_PATH "../../assets/equipment1.png"
+// ----- for images ------ //
+#define GAME_WINDOW_IMG_PATH "../../assets/equipment2.png"
+#endif
--- game/src/views/GameWindow.h
+++ game/src/views/GameWindow.h
@@ -2,11 +2,13 @@
 #define GAME_WINDOW_H
 
 #include "Window.h"
+#include "../Setting.h"
 
 class GameWindow : public Window {
+	private:
 	public:
+		GameWindow(int h, int w) : Window(h, w, GAME_WINDOW_IMG_PATH) { }
 		void drawObjects() {
+			// 
 		}
 };
-
-#endif
