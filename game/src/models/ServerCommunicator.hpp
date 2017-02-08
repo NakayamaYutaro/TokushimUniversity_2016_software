40a50f1ba912441dc6c6af1366bd5069e3ce6228
居石　峻寛 <c501606905@clpc049.(none)>
2017-02-08 21:03:17 +0900

    [clean] ServerCommunicator.hpp

--- game/src/models/ServerCommunicator.hpp
+++ game/src/models/ServerCommunicator.hpp
@@ -9,11 +9,10 @@ using namespace std;
 
 class ServerCommunicator : public Communicator {
 	private:
-		vector<struct sockaddr_in> clients;	// ゲームをプレイしているクライアントリスト
+		struct sockaddr_in client;	// ゲームをプレイしているクライアントリスト
 		unsigned int client_num;
 		sockaddr_in recv_addr;
 		char buf[BUFFER_SIZE];
-		void sendData(sockaddr_in addr, string msg);
 	public:
 		ServerCommunicator(
 				vector<CustomizedRumba> p_c_rumbas,
@@ -57,23 +56,18 @@ bool ServerCommunicator::handshake() {
 
 	memset(buf, 0, sizeof(buf));
 	addrlen = sizeof(tmp_addr);
-	// --- 送信者がclientsに登録されていなければ新規に登録する ---  //
 	if( recvfrom(recv_sock, buf, sizeof(buf), 0, (struct sockaddr*)&tmp_addr, &addrlen) >= 0 ) {
 		unsigned int i = 0;
-		for(; i < clients.size(); i++) {
-			if( clients[i].sin_addr.s_addr == tmp_addr.sin_addr.s_addr ) break;
-		} 
-		if( i == clients.size() ) {
-			tmp_addr.sin_port = htons(SERVER_DEST_PORT);
-			clients.push_back(tmp_addr);
-			// クライアントにIDを返信
-			ostringstream stream;
-			stream <<  "{\"cmd\":\"" << CMD_HANDSHAKE << "\",  \"ID\":" << i+1 << " }";
-			sendData(tmp_addr, stream.str());
-			cout << "Connected from: " << inet_ntoa(tmp_addr.sin_addr) << ':' << ntohs(tmp_addr.sin_port) << '\n' << buf << endl;
-		}
+		tmp_addr.sin_port = htons(SERVER_DEST_PORT);
+		client = tmp_addr;
+		// クライアントにIDを返信
+		ostringstream stream;
+		stream <<  "{\"cmd\":\"" << CMD_HANDSHAKE << "\",  \"ID\":" << i+1 << " }";
+		sendData(stream.str());
+		cout << "Connected from: " << inet_ntoa(tmp_addr.sin_addr) << ':' << ntohs(tmp_addr.sin_port) << '\n' << buf << endl;
+		return true;
 	}
-	return (clients.size() == client_num-1);
+	return false;
 }
 
 void* ServerCommunicator::receiveThread(void* args) {
@@ -101,15 +95,11 @@ void* ServerCommunicator::receiveThread(void* args) {
 
 }
 
-void ServerCommunicator::sendData(sockaddr_in addr, string msg) {
-	socklen_t addrlen;
-	addrlen = sizeof(addr);
-	const void* tmp_msg = msg.c_str();
-	sendto(send_sock, tmp_msg, msg.size(), 0, (struct sockaddr*)&(addr), addrlen);
-}
-
 void ServerCommunicator::sendData(string msg) {
-	for( unsigned int i = 0; i < clients.size(); i++ ) sendData(clients[i], msg);
+	socklen_t addrlen;
+	addrlen = sizeof(client);
+	const void* tmp_msg = msg.c_str();
+	sendto(send_sock, tmp_msg, msg.size(), 0, (struct sockaddr*)&(client), addrlen);
 }
 
 void ServerCommunicator::startReceiving() {
